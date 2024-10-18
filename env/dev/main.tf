c6720c04a615caf1f4c5bdf4186732e2da708c1f
Ixy-194 <ixy.194x@gmail.com>
2024-10-18 14:59:12 +0900

    ss

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -8,7 +8,7 @@ locals {
   private_subnets = ["192.168.1.32/28", "192.168.1.48/28"]
   rds_subnets     = ["192.168.1.0/28", "192.168.1.16/28"]
   azs             = ["ap-northeast-1a", "ap-northeast-1c"]
-  # service_name    = "go-api-sample-todo"
+
 }
 
 module "vpc" {
@@ -21,48 +21,3 @@ module "vpc" {
   azs             = local.azs
   env             = local.env
 }
-
-# module "alb" {
-#   source = "../../modules/alb"
-
-#   env     = local.env
-#   vpc_id  = module.vpc.vpc.vpc_id
-#   subnets = module.vpc.vpc.public_subnets
-# }
-
-# module "ecs" {
-#   source = "../../modules/ecs"
-
-#   env                  = local.env
-#   vpc_id               = module.vpc.vpc.vpc_id
-#   subnets              = module.vpc.vpc.private_subnets
-#   lb                   = module.alb.lb
-#   lb_security_group_id = module.alb.lb_security_group_id
-#   service_name         = local.service_name
-# }
-
-# module "rds" {
-#   source = "../../modules/rds"
-
-#   env                      = local.env
-#   vpc_id                   = module.vpc.vpc.vpc_id
-#   azs                      = local.azs
-#   db_subnet_group_name     = module.vpc.vpc.database_subnet_group
-#   access_allow_cidr_blocks = module.vpc.vpc.private_subnets_cidr_blocks
-# }
-
-# module "bastion" {
-#   source = "../../modules/bastion"
-
-#   env       = local.env
-#   vpc_id    = module.vpc.vpc.vpc_id
-#   subnet_id = module.vpc.vpc.private_subnets[0]
-# }
-
-# module "ecr" {
-#   source = "../../modules/ecr"
-
-#   env          = local.env
-#   service_name = local.service_name
-# }
-
